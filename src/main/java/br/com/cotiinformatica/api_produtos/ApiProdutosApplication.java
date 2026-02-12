dbece45378da374b7f1d096d7b9a079944a5d7ca
beatrizlima-tech <bealima.tech@gmail.com>
2026-02-12 00:51:53 -0300

    feat: Estrutura inicial da API com entitdades, emuns, SQL e conexão ao banco

--- src/main/java/br/com/cotiinformatica/api_produtos/ApiProdutosApplication.java
+++ src/main/java/br/com/cotiinformatica/api_produtos/ApiProdutosApplication.java
@@ -1,13 +1,41 @@
 package br.com.cotiinformatica.api_produtos;
 
+import br.com.cotiinformatica.api_produtos.factories.ConnectionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiProdutosApplication {
+public class ApiProdutosApplication implements CommandLineRunner {
+
+@Autowired
+private ConnectionFactory connectionFactory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiProdutosApplication.class, args);
+	}
+
+	/*
+		Método da interface CommandLineRunner que podemos usar para
+		executar algum tipo de rotina ou teste assim que o projeto
+		for inicializado
+	 */
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//Testar a conexão com o banco de dados
+
+		try{
+
+			connectionFactory.getConnection();
+			System.out.println("\nCONEXÃO COM O BANCO DE DADOS REALIZADOS COM SUCESSO!");
+		}
+		catch(Exception e){
+			System.out.println("\nFALHA AO CONECTAR COM O BANCO DE DADOS");
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
---
+++ src/main/java/br/com/cotiinformatica/api_produtos/entities/Produto.java
@@ -0,0 +1,52 @@
+package br.com.cotiinformatica.api_produtos.entities;
+
+import br.com.cotiinformatica.api_produtos.enums.Categoria;
+
+public class Produto {
+
+    private Integer id;
+    private String nome;
+    private Double preco;
+    private Integer quantidade;
+    private Categoria categoria;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
